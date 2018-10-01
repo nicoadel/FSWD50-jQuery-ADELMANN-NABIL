84ead0761970bdad832ff08a429375d81d63e39f
nicoarl <nico.adelmann@hotmail.com>
2018-10-01 15:10:33 +0200

    first push lol

---
+++ Adelmann/challenge/script.js
@@ -0,0 +1,28 @@
+$('.button_minus').on('click', function(e) {
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+ 
+  $input.val(value);
+ 
+});
+ 
+$('.button_plus').on('click', function(e) {
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value < 100) {
+        value = value + 1;
+    } else {
+        value =100;
+    }
+ 
+    $input.val(value);
+});
